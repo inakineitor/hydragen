8c5ca58d1584d1386f19af5f007512572d2920a4
Iñaki Arango <arangoinaki@gmail.com>
2024-11-02 21:48:13 -0700

    Put all benchmarks in one file

--- scripts/setup_python.sh
+++ scripts/setup_python.sh
@@ -7,8 +7,8 @@ conda activate hydragen
 conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install nvidia/label/cuda-12.1.0::cuda
 
-pip install packaging==24.0 ninja==1.11.1.1 --root-user-action && pip install -e . --root-user-action
+pip install packaging==24.0 ninja==1.11.1.1 --root-user-action ignore && pip install -e . --root-user-action ignore
 
-pip install -r requirements-dev.txt --root-user-action
+pip install -r requirements-dev.txt --root-user-action ignore
 
 unset CONDA_ALWAYS_YES
