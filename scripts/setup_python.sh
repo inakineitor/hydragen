7d915277e5a82e82c672e6e59a32a77fcf8753ca
Iñaki Arango <arangoinaki@gmail.com>
2024-11-03 00:28:42 -0700

    Updated setup

--- scripts/setup_python.sh
+++ scripts/setup_python.sh
@@ -3,12 +3,16 @@ export CONDA_ALWAYS_YES="true"
 # Set up python
 conda create -n hydragen python=3.11.5
 conda activate hydragen
-# conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia # TODO: Consider upgrading the Python version
-conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia
-conda install nvidia/label/cuda-12.1.0::cuda
+# conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia # NOTE: Old versions
+conda install pytorch torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia
+# conda install nvidia/label/cuda-12.1.0::cuda # NOTE: asdf
+conda install nvidia/label/cuda-12.4.0::cuda
 
-pip install packaging==24.0 ninja==1.11.1.1 --root-user-action ignore && pip install -e . --root-user-action ignore
+pip install packaging==24.0 ninja==1.11.1.1 --root-user-action ignore
+MAX_JOBS=4 pip install -e . --root-user-action ignore --no-build-isolation
 
 pip install -r requirements-dev.txt --root-user-action ignore
+
+pip install -U "huggingface_hub[cli]"
 
 unset CONDA_ALWAYS_YES
