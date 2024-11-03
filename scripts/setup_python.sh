112e7632704c39e392d9f1adbeaa22a3717ac531
Iñaki Arango <arangoinaki@gmail.com>
2024-11-02 21:16:19 -0700

    Updated setup script

--- scripts/setup_python.sh
+++ scripts/setup_python.sh
@@ -1,10 +1,14 @@
+export CONDA_ALWAYS_YES="true"
+
 # Set up python
 conda create -n hydragen python=3.11.5
 conda activate hydragen
 # conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia # TODO: Consider upgrading the Python version
-conda install -y pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia
-conda install -y nvidia/label/cuda-12.1.0::cuda
+conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install nvidia/label/cuda-12.1.0::cuda
 
-pip install packaging==24.0 ninja==1.11.1.1 && pip install -e .
+pip install packaging==24.0 ninja==1.11.1.1 --root-user-action && pip install -e . --root-user-action
 
-pip install -r requirements-dev.txt
+pip install -r requirements-dev.txt --root-user-action
+
+unset CONDA_ALWAYS_YES
